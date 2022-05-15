f565175745a80dd8c8f596df893a1722e8c9b189
Muskan <59757988+Muskan-creator@users.noreply.github.com>
2022-05-15 20:23:16 +0530

    Create containerwithwater.cpp

---
+++ Arrays/containerwithwater.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int max_h=0;
+        int r=height.size()-1;
+        while(l<r){
+            int lh=height[l];
+            int rh=height[r];
+            int min_h=min(lh,rh);
+             max_h=max(max_h,min_h*(r-l));
+            if(lh<rh){
+                l++;
+            }
+            else{
+                r--;
+            }
+            
+        }
+        return max_h;
+    }
+};
